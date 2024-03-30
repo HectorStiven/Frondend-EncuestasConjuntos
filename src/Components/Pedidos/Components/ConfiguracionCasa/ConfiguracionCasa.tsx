cb9665bda8271e1065211c9eea86802ee08f8d44
HectorStiven <vigoyahecto90@gmail.com>
2024-03-30 18:43:49 -0500

    registro de parte basica finalizado

--- src/Components/Pedidos/Components/ConfiguracionCasa/ConfiguracionCasa.tsx
+++ src/Components/Pedidos/Components/ConfiguracionCasa/ConfiguracionCasa.tsx
@@ -1,84 +1,203 @@
-import React, { useEffect, useState } from 'react';
-import { Grid, FormControl, FormControlLabel, InputLabel, MenuItem, Select, Switch, TextField } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Grid, TextField, Button, MenuItem, Select, FormControl, InputLabel, IconButton } from '@mui/material';
 import axios from 'axios';
+import { control_success } from '../../../../Elements/alertas/alertaSucces';
 import { baseUrl } from '../../../../varaibles/black';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HouseIcon from '@mui/icons-material/House';
 
-interface I_casa {
-  id: number;
+interface FormData {
+  T002nombre_casa: string;
+  T002propietario: string;
+  T002ponderacion: string;
+  id_conjunto: string;
+  id_tipo_casa: string;
+}
+
+interface TipoCasa {
+  id: string;
   T007NombreTipoo: string;
 }
 
+interface ConjuntoData {
+  id: string;
+  T003nombre: string;
+}
+
 export const ConfiguracionCasa = () => {
-  const [form, setForm] = useState({
-    tipoCasa: '',
-    numeroCasa: '',
-    activo: false
+  const [formData, setFormData] = useState<FormData>({
+    T002nombre_casa: '',
+    T002propietario: '',
+    T002ponderacion: '',
+    id_conjunto: '',
+    id_tipo_casa: ''
   });
 
-  const handleInputChange = (e: any) => {
-    const { name, value, type, checked } = e.target;
-    const newValue = type === 'checkbox' ? checked : value;
-    setForm({
-      ...form,
-      [name]: newValue
+  const [tiposCasa, setTiposCasa] = useState<TipoCasa[]>([]);
+  const [selectedTipoCasa, setSelectedTipoCasa] = useState('');
+
+  const [conjuntos, setConjuntos] = useState<ConjuntoData[]>([]);
+  const [selectedConjunto, setSelectedConjunto] = useState('');
+
+  const handleInputChange = (event:any) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
     });
   };
 
-  const [rows, setRows] = useState<I_casa[]>([]);
-  const [selectedOption, setSelectedOption] = useState<string>('');
+  const handleTipoCasaChange = (event: any) => {
+    setSelectedTipoCasa(event.target.value as string);
+  };
 
-  const fetchData = async () => {
-    try {
-      const url = `${baseUrl}/encuestas/listar-t007tipos-de-casa/`;
-      const response = await axios.get<{ data: I_casa[] }>(url);
-      setRows(response.data.data);
-    } catch (error) {
-      console.error('Error al obtener los datos:', error);
-    }
+  const handleConjuntoChange = (event: any) => {
+    setSelectedConjunto(event.target.value as string);
   };
 
   useEffect(() => {
-    fetchData();
+    const fetchTiposCasa = async () => {
+      try {
+        const url = `${baseUrl}/encuestas/listar-t007tipos-de-casa/`;
+        const response = await axios.get<{ data: TipoCasa[] }>(url);
+        setTiposCasa(response.data.data);
+      } catch (error) {
+        console.error('Error al obtener los tipos de casa:', error);
+      }
+    };
+
+    const fetchConjuntos = async () => {
+      try {
+        const url = `${baseUrl}/encuestas/listar-t003datos-conjunto/`;
+        const response = await axios.get<{ data: ConjuntoData[] }>(url);
+        setConjuntos(response.data.data);
+      } catch (error) {
+        console.error('Error al obtener los conjuntos:', error);
+      }
+    };
+
+    fetchTiposCasa();
+    fetchConjuntos();
   }, []);
 
-  const handleSelectChange = (event: any) => {
-    setSelectedOption(event.target.value as string);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+  
+      const response = await axios.post(`${baseUrl}/encuestas/crear-t002datos-casa/`, {
+        ...formData,
+        id_tipo_casa: selectedTipoCasa,
+        id_conjunto: selectedConjunto
+      });
+      console.log('Respuesta del servidor:', response.data);
+      control_success('¡Registro de casa creado correctamente!');
+    } catch (error) {
+      console.error('Error al enviar los datos:', error);
+    }
   };
 
   return (
-    <Grid container spacing={2} style={{ marginTop: 15 }}>
-      <Grid item xs={6}>
-        <FormControl fullWidth>
-          <InputLabel id="select-label">Selecciona un tipo de casa</InputLabel>
-          <Select
-            labelId="select-label"
-            id="select"
-            value={selectedOption}
-            onChange={handleSelectChange}
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={2} style={{ marginTop: 15 }}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Nombre de la Casa"
+            name="T002nombre_casa"
+            value={formData.T002nombre_casa}
+            onChange={handleInputChange}
+            required
+            InputProps={{
+              startAdornment: (
+                <IconButton>
+                  <HouseIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Propietario"
+            name="T002propietario"
+            value={formData.T002propietario}
+            onChange={handleInputChange}
+            required
+            InputProps={{
+              startAdornment: (
+                <IconButton>
+                  <AccountCircleIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            fullWidth
+            label="Ponderación"
+            name="T002ponderacion"
+            value={formData.T002ponderacion}
+            onChange={handleInputChange}
+            required
+            InputProps={{
+              startAdornment: (
+                <IconButton>
+                  <HouseIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <InputLabel id="tipo-casa-label">Tipo de Casa</InputLabel>
+            <Select
+              labelId="tipo-casa-label"
+              id="tipo-casa"
+              value={selectedTipoCasa}
+              onChange={handleTipoCasaChange}
+              required
+            >
+              {tiposCasa.map((tipoCasa) => (
+                <MenuItem key={tipoCasa.id} value={tipoCasa.id}>
+                  {tipoCasa.T007NombreTipoo}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <InputLabel id="conjunto-label">Conjunto</InputLabel>
+            <Select
+              labelId="conjunto-label"
+              id="conjunto"
+              value={selectedConjunto}
+              onChange={handleConjuntoChange}
+              required
+            >
+              {conjuntos.map((conjunto) => (
+                <MenuItem key={conjunto.id} value={conjunto.id}>
+                  {conjunto.T003nombre}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
           >
-            {rows.map((row) => (
-              <MenuItem key={row.id} value={row.id}>
-                {row.T007NombreTipoo}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+          
+            Guardar
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <TextField
-          fullWidth
-          label="Número de Casa"
-          name="numeroCasa"
-          value={form.numeroCasa}
-          onChange={handleInputChange}
-        />
-      </Grid>
-      <Grid item xs={6}>
-        <FormControlLabel
-          control={<Switch name="activo" checked={form.activo} onChange={handleInputChange} />}
-          label="Activo"
-        />
-      </Grid>
-    </Grid>
+    </form>
   );
 };
+
--- src/Components/Pedidos/Components/ConfiguracionConjunto/ConfiguracionConjunto.tsx
+++ src/Components/Pedidos/Components/ConfiguracionConjunto/ConfiguracionConjunto.tsx
@@ -1,8 +1,165 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Grid, TextField, Button, IconButton } from '@mui/material';
+import axios from 'axios';
+import { control_success } from '../../../../Elements/alertas/alertaSucces';
+import { baseUrl } from '../../../../varaibles/black';
+import SaveIcon from '@mui/icons-material/Save';
+import PhoneIcon from '@mui/icons-material/Phone';
+import PersonIcon from '@mui/icons-material/Person';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 
-export const ConfiguracionConjunto = () => {
-  return (
-    <div>ConfiguracionConjunto</div>
-  )
+interface FormData {
+  nit: string;
+  nombre: string;
+  direccion: string;
+  numeroAdministracion: string;
+  telefono: string;
 }
 
+export const ConfiguracionConjunto = () => {
+    const [formData, setFormData] = useState({
+        nit: '',
+        nombre: '',
+        direccion: '',
+        numeroAdministracion: '',
+        telefono: ''
+    });
+
+    const handleInputChange = (event:any) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      event.preventDefault();
+      try {
+          // Validar si todos los campos están llenos
+          for (const key in formData) {
+              if (Object.prototype.hasOwnProperty.call(formData, key) && formData[key as keyof FormData] === '') {
+                  alert('Por favor, completa todos los campos.');
+                  return;
+              }
+          }
+  
+          const requestData = {
+              nit: parseInt(formData.nit),
+              T003nombre: formData.nombre,
+              T003direccion: formData.direccion,
+              T003numero_administracion: parseInt(formData.numeroAdministracion),
+              T003tel: parseInt(formData.telefono)
+          };
+  
+          const response = await axios.post(`${baseUrl}/encuestas/crear-t003datos-conjunto/`, requestData);
+          console.log('Respuesta del servidor:', response.data);
+          control_success('¡Registro de conjunto creado correctamente!');
+      } catch (error) {
+          console.error('Error al enviar los datos:', error);
+      }
+  };
+  
+
+    return (
+        <Grid container spacing={2} style={{ marginTop: 15 }}>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    fullWidth
+                    label="NIT"
+                    name="nit"
+                    value={formData.nit}
+                    onChange={handleInputChange}
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton>
+                                <AccountBoxIcon />
+                            </IconButton>
+                        ),
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    fullWidth
+                    label="Nombre"
+                    name="nombre"
+                    value={formData.nombre}
+                    onChange={handleInputChange}
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton>
+                                <PersonIcon />
+                            </IconButton>
+                        ),
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    fullWidth
+                    label="Dirección"
+                    name="direccion"
+                    value={formData.direccion}
+                    onChange={handleInputChange}
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton>
+                                <LocationOnIcon />
+                            </IconButton>
+                        ),
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    fullWidth
+                    label="Número de Administración"
+                    name="numeroAdministracion"
+                    value={formData.numeroAdministracion}
+                    onChange={handleInputChange}
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton>
+                                <PhoneIcon />
+                            </IconButton>
+                        ),
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+                <TextField
+                    fullWidth
+                    label="Teléfono"
+                    name="telefono"
+                    value={formData.telefono}
+                    onChange={handleInputChange}
+                    required
+                    InputProps={{
+                        startAdornment: (
+                            <IconButton>
+                                <PhoneAndroidIcon />
+                            </IconButton>
+                        ),
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                    startIcon={<SaveIcon />}
+                >
+                    Guardar
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
